64a75f34f202a137bc3c056073e2dad902077272
YChao <290388252@qq.com>
2018-08-04 17:19:33 +0800

    [Change] Fixed mobile css

--- src/app/comPage/first/first.component.ts
+++ src/app/comPage/first/first.component.ts
@@ -9,6 +9,7 @@ import {HttpClient} from '@angular/common/http';
 import {AppProperties} from '../../app.properties';
 import {AppService} from '../../app-service';
 import * as $ from 'jquery';
+import {error} from 'selenium-webdriver';
 // import {Cooperation} from "./cooperation.form";
 @Component({
   selector: 'app-firstpage',
@@ -19,16 +20,44 @@ import * as $ from 'jquery';
 
 export class FirstComponent implements OnInit {
    public currentPic = 0;
+   public officialArticleUrl;
+   public imgUrl;
+   public articleList = [];
+  public articleList1 = [];
   constructor(private translate: TranslateService, public router: Router, private http: HttpClient,
               private appProperties: AppProperties, private appService: AppService) {
+   this.officialArticleUrl = appProperties.getUrl() + '/officialArticle/officialList ';
+   this.imgUrl = appProperties.getImgUrl() + '/articleImg/';
   }
   ngOnInit() {
+    this.appService.postData(this.officialArticleUrl, {sid: 1}).subscribe(
+      data => {
+        console.log(data);
+        for (let i = 0 ; i < 3; i++) {
+          this.articleList.push(data.returnObject[i]);
+        }
+      },
+      error1 => {
+        console.log(error1.message);
+      }
+    );
+    this.appService.postData(this.officialArticleUrl, {sid: 1}).subscribe(
+      data => {
+        console.log(data);
+        for (let i = 0 ; i < 3; i++) {
+          this.articleList1.push(data.returnObject[i]);
+        }
+      },
+      error1 => {
+        console.log(error1.message);
+      }
+    );
     setInterval(() => {
       const num = (this.currentPic + 1) % 2;
       this.currentPic = num;
     }, 3000);
   }
-  changebanner(i){
+  changebanner(i) {
     this.currentPic = i;
   }
 }
