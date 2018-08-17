9964730a9ec30499ec19dbebe01e5479c67d7a09
YChao <290388252@qq.com>
2018-08-17 09:50:51 +0800

    [Change] change some ui

--- src/app/app.properties.ts
+++ src/app/app.properties.ts
@@ -9,12 +9,12 @@ export class AppProperties {
   constructor() {
     // this.imgUrl = 'http://192.168.0.108:8769/ys_admin';
     // this.appUrl = 'http://192.168.0.108:8769/ys_admin'; // localtest
-    this.imgUrl = 'http://192.168.0.104:6662/ys_admin';
-    this.appUrl = 'http://192.168.0.104:6662/ys_admin'; // localtest
+    // this.imgUrl = 'http://192.168.0.104:6662/ys_admin';
+    // this.appUrl = 'http://192.168.0.104:6662/ys_admin'; // localtest
     // this.imgUrl = 'http://192.168.0.128:6662/ys_admin';
     // this.appUrl = 'http://192.168.0.128:6662/ys_admin'; // localtest
-    // this.imgUrl = 'http://47.106.92.82:6662/ys_admin';
-    // this.appUrl = 'http://47.106.92.82:6662/ys_admin'; // localtest
+    this.imgUrl = 'http://47.106.92.82:6662/ys_admin';
+    this.appUrl = 'http://47.106.92.82:6662/ys_admin'; // localtest
 
   }
   getUrl(): string {
